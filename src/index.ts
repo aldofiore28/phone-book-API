ffe265b22fcdd174ee2326ff69150a55b7401bc8
aldofiore28 <aldo.fiore@seccl.tech>
2022-04-22 16:39:30 +0100

    Implemented a router

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,12 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import router from './router'
 
 dotenv.config()
 
 const app = express()
 const port = process.env.APP_PORT || 5000
 
-app.get('/', (req, res) => {
-  res.status(200).send()
-})
+app.use(router)
 
 app.listen(port, () => console.log(`Running on port ${port}`))
