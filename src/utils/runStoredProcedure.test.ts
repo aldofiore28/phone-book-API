292108912006a24193d2f2cc76a87ea183e8a7b4
aldofiore28 <aldo.fiore@seccl.tech>
2022-04-22 22:44:43 +0100

    Basic test for the runStoredProcedure function

---
+++ src/utils/runStoredProcedure.test.ts
@@ -0,0 +1,44 @@
+import { StoredProcedures } from '../types'
+
+const mocks = {
+  storedProcedureName: 'a procedure' as StoredProcedures,
+  sql: {
+    Request: jest.fn()
+  },
+  execute: jest.fn()
+}
+
+jest.mock('mssql', () => ({
+  __esModule: true,
+  default: mocks.sql
+}))
+
+import { runStoredProcedure } from './runStoredProcedure'
+
+describe('runStoredProcedure', () => {
+  beforeEach(() => {
+    mocks.sql.Request.mockImplementation(() => ({
+      execute: mocks.execute
+    }))
+  })
+
+  afterEach(() => {
+    mocks.sql.Request.mockClear()
+    mocks.execute.mockClear()
+  })
+
+  it('returns data from the database', async () => {
+    const anyData = {
+      a: 1
+    }
+
+    mocks.execute.mockResolvedValue({
+      recordset: [anyData]
+    })
+
+    const result = await runStoredProcedure(mocks.storedProcedureName)
+
+    expect(mocks.execute).toHaveBeenCalledWith(mocks.storedProcedureName)
+    expect(result).toStrictEqual([anyData])
+  })
+})
