89cdb4e92cd6161166eb0e3642b95ff5050f43d1
aldofiore28 <aldo.fiore@seccl.tech>
2022-04-22 16:06:16 +0100

    Implemented app port on .env

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import express from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express()
-const port = 5000
+const port = process.env.APP_PORT || 5000
 
 app.get('/', (req, res) => {
   res.status(200).send()
