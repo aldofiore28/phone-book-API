83aa918e411d2c606487208ce2305359ef9d8489
aldofiore28 <aldo.fiore@seccl.tech>
2022-04-25 15:43:07 +0100

    app build on a function

---
+++ src/buildApp.ts
@@ -0,0 +1,35 @@
+import express, { Express } from 'express'
+import phoneNumberRouter from './router'
+import { config as sqlConfig } from 'mssql'
+
+export const buildApp = async (sql: any): Promise<Express> => {
+  const app = express()
+  const port = process.env.APP_PORT || 5000
+  const databaseOptions: sqlConfig = {
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_TARGET,
+    server: process.env.DATABASE_SERVER as string,
+    pool: {
+      max: 10,
+      min: 0,
+      idleTimeoutMillis: 30000
+    },
+    options: {
+      encrypt: true,
+      trustServerCertificate: true
+    }
+  }
+
+  app.use(phoneNumberRouter)
+
+  try {
+    await sql.connect(databaseOptions)
+    app.listen(port, () => console.log(`Running on port ${port}`))
+  } catch (error) {
+    console.log('Could not start the app')
+    console.log(error)
+  }
+
+  return app
+}
--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,7 @@
-import express from 'express'
 import dotenv from 'dotenv'
-import phoneNumberRouter from './router'
-import sql, { config as sqlConfig } from 'mssql'
+import mssql from 'mssql'
+import { buildApp } from './buildApp'
 
 dotenv.config()
 
-const app = express()
-const port = process.env.APP_PORT || 5000
-const databaseOptions: sqlConfig = {
-  user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_TARGET,
-  server: process.env.DATABASE_SERVER as string,
-  pool: {
-    max: 10,
-    min: 0,
-    idleTimeoutMillis: 30000
-  },
-  options: {
-    encrypt: true,
-    trustServerCertificate: true
-  }
-}
-
-app.use(phoneNumberRouter)
-
-export const start = async () => {
-  try {
-    await sql.connect(databaseOptions)
-    app.listen(port, () => console.log(`Running on port ${port}`))
-  } catch (error) {
-    console.log('Could not start the app')
-    console.log(error)
-  }
-}
-
-start()
+buildApp(mssql)
